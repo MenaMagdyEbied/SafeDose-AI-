411fca768c32f4a549c6e7a10bbdd6d659b770c0
Doaa Ashraf <doaaelshafei1@gmail.com>
2026-06-12 12:54:09 +0300

    Routing and notFound Component

--- SafeDose-AI/frontend/src/app/app.routes.ts
+++ SafeDose-AI/frontend/src/app/app.routes.ts
@@ -1,26 +1,75 @@
 import { Routes } from '@angular/router';
+import { Auth } from './core/auth/services/auth';
+import { AdminDashboard } from './features/admin-dashboard/admin-dashboard';
+import { CaregiverDashboard } from './features/caregiver-dashboard/caregiver-dashboard';
+import { CaregiverReview } from './features/caregiver-review/caregiver-review';
+import { DigitalCard } from './features/digital-card/digital-card';
 import { Home } from './features/home/home';
-import { authGuard } from './core/auth/guards/auth-guard';
+import { InteractionChecker } from './features/interaction-checker/interaction-checker';
+import { InteractionResults } from './features/interaction-results/interaction-results';
+import { PatientHome } from './features/patient-home/patient-home';
+import { Pricing } from './features/pricing/pricing';
+import { Profile } from './features/profile/profile';
+import { Splash } from './features/splash/splash';
+import { NotFound } from './shared/components/chat-bot/not-found/not-found';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
   {
-    path: 'patient',
-    loadComponent: () => import('./features/patient-home/patient-home').then((c) => c.PatientHome),
-    canActivate: [authGuard],
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'home',
+        component: Home,
+      },
+      {
+        path: 'splash',
+        component: Splash,
+      },
+
+      {
+        path: 'auth',
+        component: Auth,
+      },
+      {
+        path: 'patient',
+        component: PatientHome,
+      },
+      {
+        path: 'interaction-checker',
+        component: InteractionChecker,
+      },
+      {
+        path: 'interaction-results',
+        component: InteractionResults,
+      },
+      {
+        path: 'digital-card',
+        component: DigitalCard,
+      },
+      {
+        path: 'caregiver',
+        component: CaregiverDashboard,
+      },
+      {
+        path: 'caregiver-review',
+        component: CaregiverReview,
+      },
+      {
+        path: 'admin',
+        component: AdminDashboard,
+      },
+      {
+        path: 'pricing',
+        component: Pricing,
+      },
+      {
+        path: 'profile',
+        component: Profile,
+      },
+    ],
   },
-  {
-    path: 'caregiver',
-    loadComponent: () =>
-      import('./features/caregiver-dashboard/caregiver-dashboard').then(
-        (c) => c.CaregiverDashboard,
-      ),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'pricing',
-    loadComponent: () => import('./features/pricing/pricing').then((c) => c.Pricing),
-  },
-  { path: '**', redirectTo: 'home' },
+  { path: '**', component: NotFound },
 ];
